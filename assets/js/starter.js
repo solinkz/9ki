0ccb07cbfbc65c67caf1379a6cf74d96f109d0eb
Solomon Eyitene <31973759+solinkz@users.noreply.github.com>
2020-06-03 13:08:58 +0100

    Add files via upload
    added new files

---
+++ assets/js/starter.js
@@ -0,0 +1,42 @@
+// Importing JavaScript
+//
+// You have two choices for including Bootstrap's JS files—the whole thing,
+// or just the bits that you need.
+
+
+// Option 1
+//
+// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
+// import "../../node_modules/jquery/dist/jquery.min.js";
+
+
+
+// Option 2
+//
+// Import just what we need
+
+// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
+// import "../../node_modules/popper.js/dist/popper.min.js";
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/collapse.js";
+
+
+
+
+$(document).ready(function(){
+
+    $('#forpaidreport').on('click', ()=>{
+
+        if($('#forpaidreport').prop('checked') == true){
+            $('#collapsePaidreport').collapse('show');
+        }else{
+            $('#collapsePaidreport').collapse('hide');
+
+        }
+
+    })
+
+})
